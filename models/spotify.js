0ffb8c1a2888e7346ed707c6da82c5805a3766f9
DonSintaro <jarriaga.important@gmail.com>
2020-01-30 16:28:39 -0600

    Updated the spotify.js model file

--- models/spotify.js
+++ models/spotify.js
@@ -1,14 +1,97 @@
-module.exports = (sequelize, DataTypes) => {
+module.exports = function(sequelize,DataTypes){
 
-    const Model = sequelize.define("Spotify", {
-        artist_ID: DataTypes.STRING,
-        artist_name: DataTypes.STRING,
-        artist_genres: DataTypes.STRING,
-        country: DataTypes.STRING,
-        city: DataTypes.STRING,
-        listeners: DataTypes.INTEGER
-    })
+    const Genre = sequelize.define('genre', {
+        id: {type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+            },
+        genre1: {type: DataTypes.STRING, 
+                allowNull: true
+            },
+        genre2: {type: DataTypes.STRING,
+                allowNull: true
+            },
+        genre3: {type: DataTypes.STRING, 
+                allowNull: true
+            },
+        genre4: {type: DataTypes.STRING, 
+                allowNull: true
+            },
+        genre5: {type: DataTypes.STRING, 
+                allowNull: true
+            },
+        genre6: {type: DataTypes.STRING, 
+                allowNull: true
+            },
+        genre7: {type: DataTypes.STRING, 
+                allowNull: true
+            },
+        genre8: {type: DataTypes.STRING, 
+                allowNull: true
+            },
+        genre9: {type: DataTypes.STRING, 
+                allowNull: true
+            },
+        genre10: {type: DataTypes.STRING, 
+                allowNull: true
+            },
+        genre11: {type: DataTypes.STRING, 
+                allowNull: true
+            }
+    },{timestamps: false});
 
-    return Model;
+    const Artist = sequelize.define('artist', {
+        id: {type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+            },
+        artist: {type: DataTypes.STRING, 
+            allowNull: false,
+            },
+        artistID: {type: DataTypes.STRING,
+            defaultValue: false,
+            allowNull: false
+            },
+        genreID: {
+            type: Sequelize.INTEGER,
+            references: 'genres',
+            referencesKey: 'id'
+            },
+        locationID: {
+            type: Sequelize.INTEGER,
+            references: 'locations',
+            referencesKey: 'id'
+            }
+    },{timestamps: false});
 
+
+    const Location = sequelize.define('location', {
+        id: {type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+            },
+        country: {type: DataTypes.STRING, 
+            allowNull: false
+            },
+        region: {type: DataTypes.STRING,
+            defaultValue: false
+            },
+        city: {type: DataTypes.STRING, 
+            allowNull: false
+            },
+        listeners: {type: DataTypes.STRING, 
+            allowNull: false
+            }
+    },{timestamps: false});
+
+
+const Model = {
+    Artist:Artist,
+    Genre:Genre,
+    Location:Location
+}
+
+
+
+    return Model
 }
