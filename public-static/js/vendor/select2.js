fb36bb97add4b0a9ee53f021acc0e9e3b9fbf322
Matthew Reilly <matthewfreilly@gmail.com>
2020-02-03 02:57:38 -0600

    updated autofill areas
    you still need to hit spacebar for autofill to populate... I added an append genre currently tracking.

--- public-static/js/vendor/select2.js
+++ public-static/js/vendor/select2.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function() {
-    
+  $("currentlyTracking").hide(); 
   $(".js-example-data-ajax").select2({
     ajax: {
       url: "api/genres",
@@ -11,35 +11,44 @@ $(document).ready(function() {
         return {
           q: params.term, // search term
         }; 
+        
       },
       processResults: function (data, params) {
         //this logs params in console
-        console.log(params);
+        // console.log(params);
         //this logs from url + /api/genres
-        var genreList = [];
+        // var genreList = [];
         var id;
 
-$(".select2-search__field").keyup(function autoFill() {
+$(".select2-search__field").keydown(function autoFill() {
 
     $("#mapInfoDiv").empty();
 
         for (id=1; id < data.results.length - 1; id++){
          
           // genreList.push(data.results[id].text);
-          const indexOfFirst = (data.results[id].text).indexOf(params.term);
-        if(((data.results[id].text).match(params.term))){
-          $("#mapInfoDiv").append(`<option>${data.results[id].text}</option>`);
-        }
+          // const indexOfFirst = (data.results[id].text).indexOf(params.term);
+        if(params.term.length >= 3 && ((data.results[id].text).match(params.term))){
+          $("#mapInfoDiv").append(`<a class="chosenGenre" id=${id}> <option>${data.results[id].text}</option></a>`);
+          $(".chosenGenre").on("click", function(){
+          console.log(this.id)
+          $("#currentlyTracking").show();
+          $("#trackingGenre").html("currently tracking: " + this.text)
+          })
+      
           // $("#mapInfoDiv").append(`<option>${data.results[id].text}</option>`);
     
         }
-        if (
-            console.log(data.results[id].text.split('=')[1])); 
+        // if (
+            // console.log(data.results[id].text.split('=')[1]); 
           $(".js-example-matcher").select2({
-            matcher: autoFill
-          });           
-    });
+            matcher: autoFill,
+            tags: false,
 
+            
+          });           
+    }}
+)
         // parse the results into the format expected by Select2
         // since we are using custom formatting functions we do not need to
         // alter the remote JSON data, except to indicate that infinite
@@ -49,16 +58,19 @@ $(".select2-search__field").keyup(function autoFill() {
         return {
           results: data.results,
           pagination: {
-            more: (params.page * 30) < data.total_count
+            more: (params.page * 30) < data.total_count,
+          
           }
         };
       },
       cache: true
+      
     },
     placeholder: 'Search for a Genre',
     minimumInputLength: 1,
-    templateResult: formatRepo,
-    templateSelection: formatRepoSelection
+    templateResult: formatResults,
+    templateSelection: formatResultsSelection,
+   
   
   });
   // This are left over from documentation /setup... 
@@ -66,15 +78,18 @@ $(".select2-search__field").keyup(function autoFill() {
   // this is fine because I haven't configured that yet. I do like the
   // autopost to sidediv, so the exact presentation of these query
   // autofills is yet to be determined...
-  function formatRepo (repo) {
+  function formatResults (repo) {
     if (repo.loading) {
-      return repo.text;
+      // return repo.text;
     }
   
   }
   
-  function formatRepoSelection (repo) {
-    return repo.full_name || repo.text;
+  function formatResultsSelection (repo) {
+    if (repo.loading) {
+      // return repo.text;
+    }
+    // return repo.full_name || repo.text;
   }
 
 });
