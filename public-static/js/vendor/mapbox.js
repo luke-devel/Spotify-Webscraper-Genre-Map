f2e81b71dbb1e73302fc8e186f9a117b78a04cc1
Matthew Reilly <matthewfreilly@gmail.com>
2020-02-04 20:19:10 -0600

    here is an updated front end

--- public-static/js/vendor/mapbox.js
+++ public-static/js/vendor/mapbox.js
@@ -1,84 +1,72 @@
+
+
 mapboxgl.accessToken = 'pk.eyJ1IjoibWF0dGhld2ZyZWlsbHkiLCJhIjoiY2s1dW9mYmR1MWJ0cjNtb25lY240N3oxYyJ9.oUoPX11hY_Rz6ausgTENyw';
-   var map = new mapboxgl.Map({
-   container: 'map',
-   center:[70,-10],
-   zoom: 1,
+var map = new mapboxgl.Map({
+  container: 'map',
+  center: [70, -10],
+  zoom: 1,
 
-   style: 'mapbox://styles/matthewfreilly/ck68c8h6h08s61jpk4gnavuw6'
-   });
+  style: 'mapbox://styles/matthewfreilly/ck68c8h6h08s61jpk4gnavuw6'
+});
 
-   var months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December'
+var months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
 ];
 
 function filterBy(month) {
-    console.log("filtering by month" + month);
+  console.log("filtering by month" + month);
 
-    var filters = ['==', 'month', month];
-    // map.setFilter('unclustered-point', filters);
-    map.setFilter('cluster-count', filters);
-    map.setFilter('clusters', filters);
-    
-    // Set the label to the month
-    
-    document.getElementById('month').style.color = "#030303";
-    // document.getElementById('month').style.paddingLeft = ".5rem";
-    
-    document.getElementById('month').style.fontSize = "1.2rem";
-    document.getElementById('month').style.fontFamily = "font-family: 'Cabin', sans-serif;";
-    document.getElementById('month').textContent = months[month];
+  var filters = ['==', 'month', month];
+  // map.setFilter('unclustered-point', filters);
+  map.setFilter('cluster-count', filters);
+  map.setFilter('clusters', filters);
+
+  // Set the label to the month
+
+  document.getElementById('month').style.color = "#030303";
+  // document.getElementById('month').style.paddingLeft = ".5rem";
+
+  document.getElementById('month').style.fontSize = "1.2rem";
+  document.getElementById('month').style.fontFamily = "font-family: 'Cabin', sans-serif;";
+  document.getElementById('month').textContent = months[month];
 }
 
 map.on('load', function () {
   // Add a new source from our GeoJSON data and set the
   // 'cluster' option to true. GL-JS will add the point_count property to your source data.
-////////////////
+  ////////////////
+  selectlink;
 
-var data = {
-       "type": "FeatureCollection",
-       "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-       "features": [
-       { "properties": {  "point_count": 1000, "time":1451070887190}, "geometry": { "type": "Point", "coordinates": [ -151.5129, 63.1016, 0.0 ] } },
 
-       { "properties": {  "point_count": 300, "time": 1380450597361}, "geometry": { "type": "Point", "coordinates": [ -151.5129, 23.1016, 0.0 ] } },
 
-       { "properties": {  "point_count": 600, "time":1552608000000}, "geometry": { "type": "Point", "coordinates": [ -23.5129, 0.1016, 0.0 ] } },
 
-       { "properties": {  "point_count": 90, "time":1552608000000}, "geometry": { "type": "Point", "coordinates": [ -97, 33, 0.0 ] } },
-       { "properties": {  "point_count": 90, "time":1552608000000}, "geometry": { "type": "Point", "coordinates": [ -97, 33, 10.0  ] } },
-       { "properties": {  "point_count": 90, "time":1450491053360}, "geometry": { "type": "Point", "coordinates": [ -97, 33, 10.0  ] } },
-       { "properties": {  "point_count": 90, "time":1450491053360}, "geometry": { "type": "Point", "coordinates": [ -97, 33, 10.0  ] } },
-      
-      ]
-       };
+  data.features = data.features.map(function (d) {
+    d.properties.month = new Date(d.properties.time).getMonth();
+    return d;
+  });
 
-data.features = data.features.map(function(d) {
-d.properties.month = new Date(d.properties.time).getMonth();
-return d;
-});
+  console.log(data);
 
-console.log(data);
-
-//////////////////
+  //////////////////
   map.addSource('earthquakes', {
     type: 'geojson',
     // I'm leaving in this comment from documentation... I believe this is where we
     // will do things with our data queries (genre / listeners / co-ordinates /)
-    
+
     // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
     // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
-    data:data,
+    data: data,
     cluster: true,
     clusterMaxZoom: 14, // Max zoom to cluster points on
     clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
@@ -143,71 +131,100 @@ console.log(data);
 
 
   filterBy(0);
-   document.getElementById('slider')
-     .addEventListener('input', function(e) {
-         var month = parseInt(e.target.value, 10);
-         console.log(month);
-         filterBy(month);
-     
-
-     map.on('mouseenter', 'clusters', function () {
-      map.getCanvas().style.cursor = 'pointer';
-    });
-    map.on('mouseleave', 'clusters', function () {
-      map.getCanvas().style.cursor = '';
-    });
-//  inspect a cluster on click
-  map.on('click', 'clusters', function (e) {
-    var features = map.queryRenderedFeatures(e.point, {
-      layers: ['clusters']
-  })
-});
-    var clusterId = features[0].properties.cluster_id;
-    map.getSource('earthquakes').getClusterExpansionZoom(
-      clusterId,
-      function (err, zoom) {
-        if (err) return;
-
-        map.easeTo({
-          center: features[0].geometry.coordinates,
-          zoom: zoom
-        });
-      }
-    );
-  });
-});
+  document.getElementById('slider')
+    .addEventListener('input', function (e) {
+      var month = parseInt(e.target.value, 10);
+      console.log(month);
+      filterBy(month);
 
 
-      // inspect a cluster on click
+      map.on('mouseenter', 'clusters', function () {
+        map.getCanvas().style.cursor = 'pointer';
+      });
+      map.on('mouseleave', 'clusters', function () {
+        map.getCanvas().style.cursor = '';
+      });
+      //  inspect a cluster on click
       map.on('click', 'clusters', function (e) {
         var features = map.queryRenderedFeatures(e.point, {
           layers: ['clusters']
-        });
-        var clusterId = features[0].properties.cluster_id;
-        console.log(features);
-        
-        var longlat = features[0].geometry.coordinates;
-       
-        // var queryMapboxURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + longlat + ".json?access_token=pk.eyJ1IjoibWF0dGhld2ZyZWlsbHkiLCJhIjoiY2s1dW9mYmR1MWJ0cjNtb25lY240N3oxYyJ9.oUoPX11hY_Rz6ausgTENyw";
+        })
+      });
+      var clusterId = features[0].properties.cluster_id;
+      map.getSource('earthquakes').getClusterExpansionZoom(
+        clusterId,
+        function (err, zoom) {
+          if (err) return;
 
-        // Creating City and Cluster # jquery append here
-        var nodeCoordinates = document.createElement("li");
-        var textnodeCoordinates = document.createTextNode(longlat);
-            nodeCoordinates.appendChild(textnodeCoordinates);
-            document.getElementById("clickedCluster").appendChild(nodeCoordinates);
-
-        var nodePointCount = document.createElement("li");
-        var textnodePointCount = document.createTextNode(features[0].properties.point_count)
-            nodePointCount.appendChild(textnodePointCount);
-            document.getElementById("clickedCluster").appendChild(nodePointCount);
-
-        // Javascript click on point to display city 
-   
-      }) 
-
- 
+          map.easeTo({
+            center: features[0].geometry.coordinates,
+            zoom: zoom
+          });
+        }
+      );
+    });
+});
 
 
+// inspect a cluster on click
+map.on('click', 'clusters', function (e) {
+  var features = map.queryRenderedFeatures(e.point, {
+    layers: ['clusters']
+  });
+  var clusterId = features[0].properties.cluster_id;
+  console.log(features);
+
+  var longlat = features[0].geometry.coordinates;
+
+  // var queryMapboxURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + longlat + ".json?access_token=pk.eyJ1IjoibWF0dGhld2ZyZWlsbHkiLCJhIjoiY2s1dW9mYmR1MWJ0cjNtb25lY240N3oxYyJ9.oUoPX11hY_Rz6ausgTENyw";
+
+  // Creating City and Cluster # jquery append here
+  var nodeCoordinates = document.createElement("li");
+  var textnodeCoordinates = document.createTextNode(longlat);
+  nodeCoordinates.appendChild(textnodeCoordinates);
+  document.getElementById("clickedCluster").appendChild(nodeCoordinates);
+
+  var nodePointCount = document.createElement("li");
+  var textnodePointCount = document.createTextNode(features[0].properties.point_count)
+  nodePointCount.appendChild(textnodePointCount);
+  document.getElementById("clickedCluster").appendChild(nodePointCount);
+
+  // Javascript click on point to display city 
+
+});
+
+// async function addSource() {
+//   const waiting = await addDataSource();
+//   map.addSource('some id', {
+//     type: 'geojson',
+//     data: 'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_10m_ports.geojson'
+
+//   });
+// }
+
+
+window.addDataPoints = function addDataPoints() {
+
+  var data = {
+    "type": "FeatureCollection",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [
+      { "properties": { "point_count": 1000, "time": 0000000000000 }, "geometry": { "type": "Point", "coordinates": [-151.5129, 63.1016, 0.0] } },
+
+      { "properties": { "point_count": 300, "time": 1380450597361 }, "geometry": { "type": "Point", "coordinates": [-151.5129, 23.1016, 0.0] } },
+
+      { "properties": { "point_count": 600, "time": 1552608000000 }, "geometry": { "type": "Point", "coordinates": [-23.5129, 0.1016, 0.0] } },
+
+      { "properties": { "point_count": 90, "time": 1552608000000 }, "geometry": { "type": "Point", "coordinates": [-97, 33, 0.0] } },
+      { "properties": { "point_count": 90, "time": 1552608000000 }, "geometry": { "type": "Point", "coordinates": [-97, 33, 10.0] } },
+      { "properties": { "point_count": 90, "time": 1450491053360 }, "geometry": { "type": "Point", "coordinates": [-97, 33, 10.0] } },
+      { "properties": { "point_count": 90, "time": 1450491053360 }, "geometry": { "type": "Point", "coordinates": [-97, 33, 10.0] } },
+
+    ]
+  };
+
+  console.log(data);
+}
 
 
 
@@ -215,4 +232,4 @@ console.log(data);
 
 
 
-      
+
--- public-static/js/vendor/select2.js
+++ public-static/js/vendor/select2.js
@@ -1,11 +1,11 @@
 
-$(document).ready(function() {
-  $("currentlyTracking").hide(); 
+$(document).ready(function () {
+  $("currentlyTracking").hide();
 
   // THE VARIABLE BELOW IS THE INTEGER ID# of USER'S CHOSEN GENRE
   var userSelectedGenre;
-  var genreID; 
-  
+  var genreID;
+
   $(".js-example-data-ajax").select2({
     ajax: {
       url: "api/genres",
@@ -15,8 +15,8 @@ $(document).ready(function() {
       data: function (params) {
         return {
           q: params.term, // search term
-        }; 
-        
+        };
+
       },
       processResults: function (data, params) {
         //this logs params in console
@@ -25,97 +25,57 @@ $(document).ready(function() {
         // var genreList = [];
         var id;
 
-    $(".select2-search__field").keydown(function autoFill() {
+        $(".select2-search__field").keydown(function autoFill() {
 
-      $("#mapInfoDiv").empty();
-      var loopStop = 0;
-        for (id=0; id < data.results.length; id++){
-         
-          // genreList.push(data.results[id].text);
-          // const indexOfFirst = (data.results[id].text).indexOf(params.term);
- 
-          if((params.term.length >= 3) && ((data.results[id].text).match(params.term))){
-          $("#mapInfoDiv").append(`<a class="chosenGenre" id=${id}> <option>${data.results[id].text}</option></a>`);
-          $(".chosenGenre").on("click", function userGenreChoice(userSelectedGenre, genreID){
-            
-            $("#currentlyTracking").show();
-            $("#trackingGenre").html("currently mapping: " + this.text)
-            
-     
-            if (loopStop === 0){
-              genreID = this.id;
-              userSelectedGenre = this.text;
-              console.log(genreID);
-              console.log(userSelectedGenre);
-          loopStop++;
-        } else {
-          $(".chosenGenre").on("click", function (){
-            
-            loopStop = 0;
-            return loopStop;
-          });
-            // return userSelectedGenre;
-          
-          //   $.ajax({
-          //     type: "POST",
-          //     url: "api/genredata",
-          //     // data: {genreID : userSelectedGenre},
-          //     success: function(data) {
-          //       successmessage = 'Data was succesfully captured';
-          //       $("label#successmessage").text(successmessage);
-          //   },
-          //     dataType: 'json',
-          //     data: function (params) {
-          //       var postValues ={
-          //         genreID: userSelectedGenre, // search term
-          //       // }; 
-          //     }
-          //     return postValues;
-          //   }
-          // });
+          $("#mapInfoDiv").empty();
+          var loopStop = 0;
+          for (id = 0; id < data.results.length; id++) {
 
+            // genreList.push(data.results[id].text);
+            // const indexOfFirst = (data.results[id].text).indexOf(params.term);
 
-           }    
+            if ((params.term.length >= 3) && ((data.results[id].text).match(params.term))) {
+              $("#mapInfoDiv").append(`<a class="chosenGenre" id=${id} onclick=addDataPoints()> <option>${data.results[id].text}</option></a>`);
+
+            };
+          };
         });
-      };
-    };
-  });
-                // parse the results into the format expected by Select2
-                // since we are using custom formatting functions we do not need to
-                // alter the remote JSON data, except to indicate that infinite
-                // scrolling can be used
-          params.page = params.page || 1;
-  
+        // parse the results into the format expected by Select2
+        // since we are using custom formatting functions we do not need to
+        // alter the remote JSON data, except to indicate that infinite
+        // scrolling can be used
+        params.page = params.page || 1;
+
         return {
           results: data.results,
           pagination: {
             more: (params.page * 30) < data.total_count,
-          
+
           }
         };
       },
       cache: true
-    
+
     },
     placeholder: 'Search Genres',
-    
+
     minimumInputLength: 1,
     templateResult: formatResults,
     templateSelection: formatResultsSelection,
-   
+
   });
-  
+
   // This are left over from documentation /setup... 
   // These functions are currently blocking the dropdown menu for selections, and
   // this is probably preferable. 
-  function formatResults (repo) {
+  function formatResults(repo) {
     if (repo.loading) {
       // return repo.text;
     }
-  
+
   }
-  
-  function formatResultsSelection (repo) {
+
+  function formatResultsSelection(repo) {
     if (repo.loading) {
       // return repo.text;
     }
@@ -126,3 +86,32 @@ $(document).ready(function() {
 $.fn.select2.defaults.set('amdBase', 'select2/');
 $.fn.select2.defaults.set('amdLanguageBase', 'select2/i18n/');
 
+$(document).on("click", ".chosenGenre", function userGenreChoice(userSelectedGenre, genreID) {
+  $("#currentlyTracking").show();
+  $("#trackingGenre").html("currently mapping: " + this.text)
+  addDataPoints();
+  genreID = this.id;
+  userSelectedGenre = this.text;
+  console.log(genreID);
+  console.log(userSelectedGenre);
+
+  map.addSource('some id', {
+    type: 'geojson',
+    data: {
+        "type": "FeatureCollection",
+        "features": [{
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -76.53063297271729,
+                    39.18174077994108
+                ]
+            }
+        }]
+    }
+ });
+
+
+});
