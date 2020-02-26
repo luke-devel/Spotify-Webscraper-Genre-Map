610f1c50ad5f86837180210c3873da16836f7dd4
luke-ols0 <luke.olson@outlook.com>
2020-02-26 16:09:13 -0600

    working with npm queue package, with adjustable concurrency. writes to a .csv file with header.

--- controllers/scrapeEveryGenre.js
+++ controllers/scrapeEveryGenre.js
@@ -1,8 +1,10 @@
 // Autoscraper to scrape http://everynoise.com/new_releases_by_genre.cgi using cheerio
 // to populate json with with spotify ID's for artists for all ~ 3000 Spotify genres
 
-const request = require('request');
+var Promise = require("bluebird");
 const cheerio = require('cheerio');
+var queue = require('queue')
+
 var rp = require('request-promise');
 
 const fs = require('fs');
@@ -12,7 +14,6 @@ const fs = require('fs');
 
 (async function main() {
     try {
-
         var options = {
             uri: 'http://everynoise.com/new_releases_by_genre.cgi',
             transform: function (body) {
@@ -20,174 +21,77 @@ const fs = require('fs');
             }
         };
 
-        rp(options)
-            .then(function ($) {
-                try {
-                    var urls = [];
-                    $('.genre a').each(async (i, el) => {
-                        try {
+        const $ = await rp(options);
+        var urls = [];
+        const ids = {};
 
-                            if (i >= 234) {
-                                const link = $(el).attr('href');
-                                var url = `http://everynoise.com/new_releases_by_genre.cgi${link}`;
-                                urls.push(url);
-                            }
+        $('.genre a').each((i, el) => {
+            // must start at iteration 238 to scrape genres at beginning of alphebetical list
+            if (i >= 238) {
+                const link = $(el).attr('href');
+                var url = `http://everynoise.com/new_releases_by_genre.cgi${link}`;
+                urls.push(url);
+            }
 
-                        }
-                        catch (e) {
-                            console.log('err in .genre a loop: ' + e.message);
-                        }
-                    })
-                    console.log('done the .genre a loop');
-                    return urls;
+        })
+        console.log(`url scraping complete - urls[] length is ${urls.length}`)
+        console.log(`First genre url to scrape is: ${urls[0]}`);
+
+        fs.writeFile('./spotifyIDsFinal.csv', `artistID,\n`, function (err) {
+            console.log(`.csv written to with header`);
+            if (err) { console.log(err); }
+        })
+        const requests = urls.map(url => {
+            var options = {
+                uri: url,
+                transform: function (body) {
+                    return cheerio.load(body);
                 }
-                catch (e) {
-                    console.log('err in first loop: ' + e.message);
-                }
-            }).then(function (urls) {
-                try {
-                    var ids = [];
-                    console.log(`urls length is: ${urls.length}`);
+            };
+
+            return async () => {
+
+                const $ = await rp(options);
 
 
-                    // ~2467 times
-                    let completed_requests = 0;
-                    for (let i = 0; i < 100; i++) {
-                        try {
+                let twoCount = 0;
+                $('.albumbox.album a').each((j, ele) => {
 
-                            var options2 = {
-                                uri: urls[i],
-                                transform: function (body) {
-                                    return cheerio.load(body);
-                                }
-                            };
+                    const artistID = $(ele).attr('href').slice(29, -10);
+                    ///
+                    // console.log(artistID);
 
-                            rp(options2)
-                                .then(async function ($) {
-                                    //     Process html like you would with jQuery...
-                                    // loops twice
-                                    $('.albumbox.album a').each(await function (j, ele) {
-                                        try {
-                                            const artistLink = $(ele).attr('href').slice(29, -10);
-                                            if (j < 3 && artistLink) {
-                                                ids.push(artistLink);
-                                                completed_requests++;
-
-                                                if (completed_requests == 200) {
-                                                    // All download done, process responses array
-                                                    console.log(ids);
-                                                    console.log(ids.length);
-
-                                                    var result = JSON.stringify(ids);
-
-                                                    fs.writeFile('./spotifyIDs4.json', result, function (err) {
-                                                        console.log("The file was saved!");
-                                                        if (err) { console.log(err); }
-                                                    })
-                                                }
-
-                                            }
-                                        }
-                                        catch{
-                                            console.log(`err at albumbox album a - j: ${j}`);
-
-                                        }
-                                    })
-                                })
-                                .catch(function (err) {
-                                });
-                        }
-                        catch{
-                            console.log(`err at i: ${i} in 2700 for loop`);
-
+                    // checks if artistID is not in array already
+                    if (artistID) {
+                        if (!ids[artistID] && twoCount < 2) {
+                            ids[artistID] = true;
+                            twoCount++;
+                            fs.appendFile('./spotifyIDsFinal.csv', `${artistID},\n`, function (err) {
+                                console.log(`.csv written to with Spotify artist ID: ${artistID}`);
+                                if (err) { console.log(err); }
+                            })
                         }
                     }
+                })
 
+            }
+        })
 
-                }
-                catch{
-                    console.log(`err in new function`);
-                }
+        // defines queue, concurrency value is how many requests to be made at once
+        // higher the concurrency = more ram usage
+        var q = queue({ results: [], concurrency: 10 });
 
-            }).then(function (ids) {
+        q.push(...requests)
+        q.on('success', function (result, job) {
+        })
 
-
-
-            })
-            .catch(function (err) {
-
-            });
+        // begin processing, get notified on end / failure
+        q.start(function (err) {
+            if (err) throw err
+            console.log('all done:', q.results)
+        })
 
     } catch (e) {
-        console.log('main function err: ' + e.message);
+        console.log(e)
     }
-
 })();
-
-
-// var options = {
-//     uri: 'http://everynoise.com/new_releases_by_genre.cgi',
-//     transform: function (body) {
-//         return cheerio.load(body);
-//     }
-// };
-
-// rp(options)
-//     .then(function ($) {
-//         // Process html like you would with jQuery...
-//         const genre_a = $('.genre a');
-//         console.log(genre_a.length);
-
-//         //loops 3000 times
-
-//         // $('.genre a').each((i, el) => {
-//         //     // ignores first 4 links
-//         //     // if (i >= 4 && i <= 5) {
-//         //     if (i == 4) {
-//         //         //const item = $(el).text();
-//         //         const link = $(el).attr('href');
-//         //         console.log(`http://everynoise.com/new_releases_by_genre.cgi${link}\n`);
-//         //         var url = `http://everynoise.com/new_releases_by_genre.cgi${link}`;
-//         //         toDo(url);
-//         //     }
-
-//         // });
-//         //
-//     })
-//     .catch(function (err) {
-//         // Crawling failed or Cheerio choked...
-//     });
-// //
-
-// async function toDo(url) {
-//     var options2 = {
-//         uri: url,
-//         transform: function (body) {
-//             return cheerio.load(body);
-//         }
-//     };
-
-//     await rp(options2)
-//         .then(function ($) {
-//             // Process html like you would with jQuery...
-//             // loops twice
-//             $('.albumbox.album a').each((j, ele) => {
-//                 const artistLink = $(ele).attr('href').slice(29, -10);
-//                 if (j < 3 && artistLink) {
-//                     console.log(`Spotify id: ${artistLink}`);
-//                 }
-//                 // fs.File('./result.json', `${html} \n`, function (err) {
-//                 //     if (err) { console.log(err); }
-//                 // })
-//             })
-//         })
-//         .catch(function (err) {
-//             // Crawling failed or Cheerio choked...
-//         });
-// }
-
-
-
-
-
-
